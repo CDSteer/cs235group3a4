0d108f72f1048573d1ef3608cad5456a24064a56
Cameron Steer <cdsteer94@gmail.com>
2014-02-11 20:41:10 +0000

    Merge branch 'maingui'

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,85 @@
 /**
- * @mainpage Connect 4 and Othello
- *
- * Description of the project
- * The games Connect 4 and Othello <br>
- *
- * @author Cameron Steer
+ * @file 	Main.java
+ * @author 	cdsteer, jidavies
+ * @date	28/01/2014
+ * @see		http://zetcode.com/tutorials/javaswingtutorial/firstprograms/
+ * 
+ * Main class for displaying gui splash
  */
-
-/**
- * @file Main.java
- *
- * @brief file the has the class Main which contains the main method to start the program
- *
- * @author Cameron Steer
- *
- * @date 01.02.2014
- **/
 
 import org.lwjgl.opengl.Display;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;  
+import java.awt.*;  
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
 
-/**
- * @class Main
- *
- * @brief Class contains the main method to start the program
- *
- * This class sets up the game objects and starts the game loop, we also declare a static constant for our frames
- * games frame rate this is set to 60.
- */
-public class Main {
-
-    /**
-    *<A short one line description>
-    *
-    *<Longer description>
-    *<May span multiple lines or paragraphs as needed>
-    *
-    *@param  Description of method's or function's input parameter
-    *@param  ...
-    *@return Description of the return value
-    */
-    public static void main(String args[]) throws InterruptedException, IOException {
-        Scanner scanner = new Scanner(System.in);
-        Time time = new Time();
-        time.setUpTimer();
-        AbstractDisplay display = new C4Display();
+public class Main extends JFrame {
+	
+	public Main() {
+		//call GUI method
+		initGUI();
+	}
+	
+	public void initGUI() {		
+		AbstractDisplay display = new C4Display();
         AbstractGame game = new C4Game();
         AbstractInput gameInput = new C4Input();
+        
+		//2 cols 1 row JPanel
+		JPanel panel = new JPanel(new GridLayout(1,2));
+	       getContentPane().add(panel);
 
-        //get user game selection
-        System.out.print("Enter game: ");
-        String userGame = scanner.next();
+	       //init buttons and fancy stuff
+	       JButton c4Button = new JButton("Play Connect 4!");
+	       JButton othButton = new JButton("Play Othello!");
+	       c4Button.setFont(new Font("Arial", Font.BOLD, 34));
+	       othButton.setFont(new Font("Arial", Font.BOLD, 34));
+	       c4Button.setToolTipText("Click Me to Play Connect 4!");
+	       othButton.setToolTipText("Click Me to Play Othello! (Although we haven't done it yet..)");
+	       
+	       //c4 button action listener
+	       c4Button.addActionListener(new ActionListener() {
+	           @Override
+	           public void actionPerformed(ActionEvent event) {
+	               initC4();
+	          }
+	       });
+	       
+	     //othello button action listener
+	       othButton.addActionListener(new ActionListener() {
+	           @Override
+	           public void actionPerformed(ActionEvent event) {
+	               initOthello();
+	          }
+	       });
 
-        if (userGame.equals("c4")){
-            display = new C4Display();
-            game = new C4Game();
-            gameInput = new C4Input();
-        }
-        if (userGame.equals("oth")){
-            display = new OthDisplay();
-            game = new OthGame();
-            gameInput = new OthInput();
-        }
-
+	       //add buttons to the JPanel
+	       panel.add(c4Button);
+	       panel.add(othButton);
+		
+		//init JFrame
+	    setTitle("A4 Partial Implementation : Group 3 ");
+        setSize(600, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);       
+	}	
+	
+	public void initC4() {
+		AbstractDisplay display = new C4Display();
+        AbstractGame game = new C4Game();
+        AbstractInput gameInput = new C4Input();
+        Time time = new Time();
+        time.setUpTimer();
+        
+        display = new C4Display();
+        game = new C4Game();
+        gameInput = new C4Input();
+        
         display.setUpDisplay();
         display.setUpOpenGL();
         Square.setTexture();
@@ -74,14 +89,31 @@ public class Main {
             game.gameLoop(game, time.getDelta());
             display.render(game.getBoard(), game.getCurrentCounter(), game.getOnScreenCounters());
             Display.update();
-            Display.sync(time.getFrameRate());
+            Display.sync(FRAMESPERSEC);
             if (Display.isCloseRequested()) {
                 game.setRunning(false);
             }
         }
-        game.getBoard().unBind();
         Display.destroy();
         System.exit(0);
+	}
+	
+	public void initOthello() {
+		System.out.println("No Othello game yet, exiting...");
+        System.exit(0);
+	}
+
+    public static void main(String args[]) throws InterruptedException, IOException {
+      
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Main splash = new Main();
+                splash.setVisible(true);
+            }
+        });
     }
+
+    private static final int FRAMESPERSEC = 60;
 
 }
