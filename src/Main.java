f882763c4ab9d5ff43531f8aeb3bd9bdc0be5294
Jamie I Davies <jamie@jidavies.co.uk>
2014-02-11 20:22:34 +0000

    Completely rewritten Main class functions, adding a Swing JFrame along with two large buttons. Methods are now implemented to call each game board, as I need this for the action listener to work.

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,85 @@
 /**
- *
- * @author cdsteer, jidavies
- *         - created 28/01/2014
- * @version *.*
+ * @file 	Main.java
+ * @author 	cdsteer, jidavies
+ * @date	28/01/2014
+ * @see		http://zetcode.com/tutorials/javaswingtutorial/firstprograms/
+ * 
+ * Main class for displaying gui splash
  */
 
 import org.lwjgl.opengl.Display;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;  
+import java.awt.*;  
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
 
-public class Main {
-
-    public static void main(String args[]) throws InterruptedException, IOException {
-        Scanner scanner = new Scanner(System.in);
-        Time time = new Time();
-        time.setUpTimer();
-        AbstractDisplay display = new C4Display();
+public class Main extends JFrame {
+	
+	public Main() {
+		//call GUI method
+		initGUI();
+	}
+	
+	public void initGUI() {		
+		AbstractDisplay display = new C4Display();
         AbstractGame game = new C4Game();
         AbstractInput gameInput = new C4Input();
+        
+		//2 cols 1 row JPanel
+		JPanel panel = new JPanel(new GridLayout(1,2));
+	       getContentPane().add(panel);
 
-        //get user game selection
-        System.out.print("Enter game: ");
-        String userGame = scanner.next();
+	       //init buttons and fancy stuff
+	       JButton c4Button = new JButton("Play Connect 4!");
+	       JButton othButton = new JButton("Play Othello!");
+	       c4Button.setFont(new Font("Arial", Font.BOLD, 34));
+	       othButton.setFont(new Font("Arial", Font.BOLD, 34));
+	       c4Button.setToolTipText("Click Me to Play Connect 4!");
+	       othButton.setToolTipText("Click Me to Play Othello! (Although we haven't done it yet..)");
+	       
+	       //c4 button action listener
+	       c4Button.addActionListener(new ActionListener() {
+	           @Override
+	           public void actionPerformed(ActionEvent event) {
+	               initC4();
+	          }
+	       });
+	       
+	     //othello button action listener
+	       othButton.addActionListener(new ActionListener() {
+	           @Override
+	           public void actionPerformed(ActionEvent event) {
+	               initOthello();
+	          }
+	       });
 
-        if (userGame.equals("c4")){
-            display = new C4Display();
-            game = new C4Game();
-            gameInput = new C4Input();
-        }
-        if (userGame.equals("oth")){
-            display = new OthDisplay();
-            game = new OthGame();
-            gameInput = new OthInput();
-        }
-
+	       //add buttons to the JPanel
+	       panel.add(c4Button);
+	       panel.add(othButton);
+		
+		//init JFrame
+	    setTitle("A4 Partial Implementation : Group 3 ");
+        setSize(600, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);       
+	}	
+	
+	public void initC4() {
+		AbstractDisplay display = new C4Display();
+        AbstractGame game = new C4Game();
+        AbstractInput gameInput = new C4Input();
+        Time time = new Time();
+        time.setUpTimer();
+        
+        display = new C4Display();
+        game = new C4Game();
+        gameInput = new C4Input();
+        
         display.setUpDisplay();
         display.setUpOpenGL();
         Square.setTexture();
@@ -52,6 +96,22 @@ public class Main {
         }
         Display.destroy();
         System.exit(0);
+	}
+	
+	public void initOthello() {
+		System.out.println("No Othello game yet, exiting...");
+        System.exit(0);
+	}
+
+    public static void main(String args[]) throws InterruptedException, IOException {
+      
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Main splash = new Main();
+                splash.setVisible(true);
+            }
+        });
     }
 
     private static final int FRAMESPERSEC = 60;
