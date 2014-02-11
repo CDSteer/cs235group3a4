0b14ad5621d9871050c7fdb4f83d4248116ae68d
Jamie I Davies <jamie@jidavies.co.uk>
2014-02-11 21:28:48 +0000

    doxygen header

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 /**
+ * @mainpage	Software Engineering 2 - Group3 PDM Assignment 4
+ * 
  * @file 	Main.java
  * @author 	Jamie Irving Davies, Cameron Steer
- * @date	28/01/2014
+ * @date	January 28, 2014
  * @see		http://zetcode.com/tutorials/javaswingtutorial/firstprograms/
+ * @brief	Main class for GUI Splash
+ * @details	Main class for displaying gui splash window to allow the player to select which game to play
  * 
- * Main class for displaying gui splash window to allow the player to select which game to play
  */
 
 import org.lwjgl.opengl.Display;
