f663165bd1c0c7907d26c1bbc750fcf6974656e0
Jamie I Davies <jamie@jidavies.co.uk>
2014-02-25 21:18:02 +0000

    MainWindow class added
    Added MainWindow class framework for the class to handle all the GUI
    stuff

---
+++ src/MainWindow.java
@@ -0,0 +1,22 @@
+/*
+ * @file MainWindow.java
+ * @author Jamie Davies
+ * @date February 25 2014
+ * @version 1.0
+ * @see Main.java
+ * @brief class for storing all gui functions for Main class
+ */
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;  
+import java.awt.*;
+import java.io.IOException;
+
+
+public class MainWindow extends JFrame {
+	
+	     
+
+}
