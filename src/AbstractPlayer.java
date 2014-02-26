13e343fab1f5f1d4bbbb099b9b7989b0bb6538d8
CurtisLewis <gunny_mun@hotmail.co.uk>
2014-02-26 22:07:09 +0000

    Updated AbstractPlayer with a score variable for Othello and doxygen comments

--- src/AbstractPlayer.java
+++ src/AbstractPlayer.java
@@ -1,16 +1,30 @@
 /**
  * Created with IntelliJ IDEA.
  *
- * @author cdsteer
- *         - created 01/02/2014
- * @version *.*
+ *@date    Verified and Updated by Design Manager Curtis on 23rd Feb 2014
+ *@see
+ *@author  Cameron Steer and Curtis Lewis
+ *@brief   This class is AbstractPlayer which contain private member variables for name and score
+ *         as well as getters and setters for these variables.
+ *@version *1.0.1*
  */
 public abstract class AbstractPlayer {
 
-    private int name;
+    private int m_name;
+    private int m_score;
 
     public AbstractPlayer(int name){
-        this.name = name;
+        this.m_name = name;
     }
+
+    public void set_score(int score) {
+        this.m_score = score;
+    }
+
+    public int isRunning() {
+        return m_score;
+    }
+
+
 
 }
