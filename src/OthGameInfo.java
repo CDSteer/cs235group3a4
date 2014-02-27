88acb5ee70c298b291d63059d62f7badaf4901be
CJenkins91 <chrisalanjenkins@gmail.com>
2014-02-27 01:04:24 +0000

    Update OthGameInfo.java

--- src/OthGameInfo.java
+++ src/OthGameInfo.java
@@ -10,25 +10,25 @@ import java.awt.Font;
  */
 public class OthGameInfo extends AbstractGameInfo{
 
-    //private Font scoreFont;
-    private boolean player1Wins;
-    private boolean player2Wins;
-
 //    Font DisplayFont = new Font("Arial", Font.BOLD, 30);
 //    TrueTypeFont scorefont = new TrueTypeFont(DisplayFont, false);
 //    Font awtFont = new Font("Arial", Font.BOLD, 70);
-    private Font font;
     private TrueTypeFont fontX;
+    private TrueTypeFont fontError;
+    private Font font;
+
 
     public OthGameInfo(){
         font = new Font("Arial", Font.BOLD, 30);
+
     }
 
     public void draw(AbstractGame game){
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         Color.white.bind();
         fontX = new TrueTypeFont(font, false);
-        fontX.drawString(100, 400, "Welcome to Othello");
+
+        //fontX.drawString(100, 400, "Welcome to Othello");
         String turnText = "Player " + game.getTurn() + "'s turn";
         fontX.drawString(400, 200, turnText);
 
@@ -36,32 +36,28 @@ public class OthGameInfo extends AbstractGameInfo{
         //in these strings
         fontX.drawString(400, 100, "Player 1: ");
         fontX.drawString(400, 150, "Player 2: ");
+
         GL11.glDisable(GL11.GL_TEXTURE_2D);
 
     }
 
-
-
-    /**
-     *Sets the Font
-     */
-    /**
-    public static void GameInfo(){
-
-        if (player1Wins == true){
-
-        } else if (player2Wins == true){
-
-        }
+    public void invalidMoveMessage(){
+        fontError = new TrueTypeFont(font, false);
+        fontError.drawString(130, 10, "Invalid move");
     }
-    **/
-    /**
-     *Draws the Font
-     */
-    /**
-    public static void draw(private fontArial){
 
+    public void p1WinsMessage(){
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        fontX = new TrueTypeFont(font, false);
+        fontX.drawString(200, 380, "Player 1 Wins!");
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
     }
-    **/
+
+    public void p2WinsMessage(){
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        fontX = new TrueTypeFont(font, false);
+        fontX.drawString(200, 380, "Player 2 Wins!");
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+    }
 
 }
